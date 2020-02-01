40434e82fa7b2b1503548ce87aef657e539902bc
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-02-01 16:35:35 -0500

    up to v2.1.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
-VER=2.0.0
-URL=https://www.apache.org/dist/storm/apache-storm-${VER}/apache-storm-${VER}.tar.gz.sha512
+MIRROR=https://www.apache.org/dist/storm
 
-printf "  # %s\n" $URL
-printf "  '%s': sha512:%s\n" $VER `curl -sSL $URL | tr '\n' ' ' | tr -s '[:blank:]' ' ' | sed -e 's/ //g' | awk -F ':' '{print $2}'`
+dl_ver() {
+    local ver=$1
+    local url=$MIRROR/apache-storm-${ver}/apache-storm-${ver}.tar.gz.sha512
+    printf "  # %s\n" $url
+    printf "  '%s': sha512:%s\n" $ver $(curl -sSL $url | tr '\n' ' ' | tr -s '[:blank:]' ' ' | sed -e 's/ //g' | awk -F ':' '{print $2}')
+}
 
+dl_ver ${1:-2.1.0}
