3498b687b650f922b4de9f9ca4dcc437375afd2a
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-03-26 20:54:03 -0400

    storm:2.4.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
+set -e
 MIRROR=https://www.apache.org/dist/storm
 
 dl_ver() {
     local ver=$1
     local url=$MIRROR/apache-storm-${ver}/apache-storm-${ver}.tar.gz.sha512
     printf "  # %s\n" $url
-    printf "  '%s': sha512:%s\n" $ver $(curl -sSL $url | tr '\n' ' ' | tr -s '[:blank:]' ' ' | sed -e 's/ //g' | awk -F ':' '{print $2}')
+    printf "  '%s': sha512:%s\n" $ver $(curl -sSLf $url | tr '\n' ' ' | tr -s '[:blank:]' ' ' | sed -e 's/ //g' | awk -F ':' '{print $2}')
 }
 
-dl_ver ${1:-2.3.0}
+dl_ver ${1:-2.4.0}
